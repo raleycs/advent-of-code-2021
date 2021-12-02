d13a1e3d99b914bc91bf07795157f21ba1644c83
Christopher Raley <cr2fv@virginia.edu>
2021-12-02 04:34:39 -0500

    add solution for day 2 part 2

---
+++ day2/part2/main.go
@@ -0,0 +1,51 @@
+package main
+
+import(
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    fmt.Printf("Reading in input.txt...\n")
+
+    // Open "input.txt"
+    buffer, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer buffer.Close()
+
+    horizontal := 0 // temporary var to hold horizontal position
+    depth := 0 // var to hold depth
+    aim := 0 // var to hold aim
+
+    // Create Scanner to read in file line by line
+    scanner := bufio.NewScanner(buffer)
+    for scanner.Scan() {
+        command := strings.Fields(scanner.Text()) // split string by whitespace
+        t := command[0]  // forward/up/down
+        val, err := strconv.Atoi(command[1]) // convert string into int
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        if t == "forward" {
+            horizontal += val
+            depth += aim * val
+        } else if t == "up" {
+            aim -= val
+        } else if t == "down" {
+            aim += val
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Solution: %d\n", horizontal * depth)
+}
