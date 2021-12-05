7e5927676f059a0c182302dfb29cbd41b4fd8f5b
Christopher Raley <cr2fv@virginia.edu>
2021-12-05 18:17:19 -0500

    add solutio for day 5 part 1

---
+++ day5/part1/main.go
@@ -0,0 +1,89 @@
+package main
+
+import(
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    "strconv"
+)
+
+func main() {
+    fmt.Printf("Reading in input.txt...\n")
+
+    // Open "input.txt"
+    buffer, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer buffer.Close()
+
+    // create map to keep track of danger points
+    danger := make(map[string]int)
+
+    // Create Scanner to read in file line by line
+    scanner := bufio.NewScanner(buffer)
+    for scanner.Scan() {
+        endpoints := strings.Split(scanner.Text(), " -> ")
+        coordinates := [][]string{}
+        for _, pair := range(endpoints) {
+            coordinates = append(coordinates, strings.Split(pair, ","))
+        }
+
+        // parse coordinates as strings
+        x0, err := strconv.Atoi(coordinates[0][0])
+        if err != nil {
+            log.Fatal(err)
+        }
+        y0, err := strconv.Atoi(coordinates[0][1])
+        if err != nil {
+            log.Fatal(err)
+        }
+        x1, err := strconv.Atoi(coordinates[1][0])
+        if err != nil {
+            log.Fatal(err)
+        }
+        y1, err := strconv.Atoi(coordinates[1][1])
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        // fill out maps with any danger zones
+        if coordinates[0][0] == coordinates[1][0] {
+            if y1 >= y0 {
+                for i := y0; i <= y1; i++ {
+                    danger[coordinates[0][0] + "," + strconv.Itoa(i)] += 1
+                }
+            } else {
+                for i := y0; i >= y1; i-- {
+                    danger[coordinates[0][0] + "," + strconv.Itoa(i)] += 1
+                }
+            }
+        } else if coordinates[0][1] == coordinates[1][1] {
+            if x1 >= x0 {
+                for i := x0; i <= x1; i++ {
+                    danger[strconv.Itoa(i) + "," + coordinates[1][1]] += 1
+                }
+            } else {
+                for i := x0; i >= x1; i-- {
+                    danger[strconv.Itoa(i) + "," + coordinates[1][1]] += 1
+                }
+            }
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+    overlaps := 0
+    for _, v := range(danger) {
+        if v >= 2 {
+            overlaps += 1
+            // fmt.Printf("%s : %d\n", k, v)
+        }
+    }
+
+    fmt.Printf("Total overlaps: %d\n", overlaps)
+}
