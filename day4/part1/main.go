f78d11b58c7a99f3863d3674cc54dabd1c38eda6
Christopher Raley <cr2fv@virginia.edu>
2021-12-06 04:06:43 -0500

    add solution for day 4 part 1

--- day4/part1/main.go
+++ day4/part1/main.go
@@ -11,19 +11,19 @@ import(
 
 // readInput reads input.txt and
 // returns all binaries in a slice
-func readInput()([]string, [][][]string) {
+func readInput()([]int, [][][]int) {
     // Open "input.txt"
-    // buffer, err := os.Open("input.txt")
-    buffer, err := os.Open("test.txt")
+    buffer, err := os.Open("input.txt")
+    // buffer, err := os.Open("test.txt")
     if err != nil {
         log.Fatal(err)
     }
     defer buffer.Close()
 
 
-    boards := [][][]string{} // slice of 2D slices to hold bingo boards
-    board := [][]string{} // new bingo board to add
-    drawn := []string{} // slice of numbers drawn
+    boards := [][][]int{} // slice of 2D slices to hold bingo boards
+    board := [][]int{} // new bingo board to add
+    drawn := []int{} // slice of numbers drawn
 
     // Create Scanner to read file and fill bingo boards
     readBoard := false // determine when we should start reading bingo boards
@@ -31,14 +31,28 @@ func readInput()([]string, [][][]string) {
     for scanner.Scan() {
         if readBoard {
             if len(board) != 5 && scanner.Text() != "" {
-                board = append(board, strings.Split(strings.Join(strings.Fields(scanner.Text()), " "), " "))
+                i := []int{}
+                for _, s := range(strings.Split(strings.Join(strings.Fields(scanner.Text()), " "), " ")) {
+                    num, err := strconv.Atoi(s)
+                    if err != nil {
+                        log.Fatal(err)
+                    }
+                    i = append(i, num)
+                }
+                board = append(board, i)
             } else if len(board) == 5 && scanner.Text() == "" {
                 boards = append(boards, board)
                 board = nil // clear bingo board
             }
         } else {
             // read first line as drawn numbers
-            drawn = strings.Split(strings.Join(strings.Fields(scanner.Text()), " "), ",")
+            for _, s := range(strings.Split(strings.Join(strings.Fields(scanner.Text()), " "), ",")) {
+                num, err := strconv.Atoi(s)
+                if err != nil {
+                    log.Fatal(err)
+                }
+                drawn = append(drawn, num)
+            }
             readBoard = true
         }
     }
@@ -51,31 +65,24 @@ func readInput()([]string, [][][]string) {
 }
 
 // getScore counts total score
-func getScore(board [][]string, winningNumber string) int {
+func getScore(board [][]int, winningNumber int) int {
     score := 0
     for x := 0; x < 5; x++ {
         for y := 0; y < 5; y++ {
-            if board[x][y] != "x" {
-                points, err := strconv.Atoi(board[x][y])
-                if err != nil {
-                    log.Fatal(err)
-                }
-                score += points
+            if board[x][y] != -1 {
+                score += board[x][y]
             }
         }
     }
-    winner, err := strconv.Atoi(winningNumber)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return winner * score
+    fmt.Printf("Calculating final score: %d * %d\n", score, winningNumber)
+    return score * winningNumber
 }
 
 // get board state
-func viewBoard(board [][]string) {
+func viewBoard(board [][]int) {
     for x := 0; x < 5; x++ {
         for y := 0; y < 5; y++ {
-            fmt.Printf("%s \t", board[x][y])
+            fmt.Printf("%d \t", board[x][y])
         }
         fmt.Println()
     }
@@ -83,15 +90,19 @@ func viewBoard(board [][]string) {
 
 // check if we have a horizontal bingo
 // return true if we have a bingo
-func checkHorizontal(board [][]string) bool {
+func checkHorizontal(board [][]int) bool {
     for y := 0; y < 5; y++ {
-        if board[y][0] == "x" {
+        if board[y][0] == -1 {
+            allMarked := true
             for x := 0; x < 5; x++ {
-                if board[y][x] != "x" {
-                    return false
+                if board[y][x] != -1 {
+                    allMarked = false
+                    break
                 }
             }
-            return true
+            if allMarked {
+                return true
+            }
         }
     }
     return false
@@ -99,15 +110,20 @@ func checkHorizontal(board [][]string) bool {
 
 // check if we have a vertical bingo
 // return true if we have a bingo
-func checkVertical(board [][]string) bool {
+func checkVertical(board [][]int) bool {
     for x := 0; x < 5; x++ {
-        if board[0][x] == "x" {
+        if board[0][x] == -1 {
+            allMarked := true
             for y := 0; y < 5; y++ {
-                if board[y][x] != "x" {
-                    return false
+                fmt.Println(board[y][x])
+                if board[y][x] != -1 {
+                    allMarked = false
+                    break
                 }
             }
-            return true
+            if allMarked {
+                return true
+            }
         }
     }
     return false
@@ -115,13 +131,13 @@ func checkVertical(board [][]string) bool {
 
 // check if we have a diagonal bingo
 // return true if we have a bingo
-func checkDiagonal(board [][]string) bool {
+func checkDiagonal(board [][]int) bool {
     // top left to bottom right
-    if board[0][0] == "x" && board[1][1] == "x" && board[2][2] == "x" && board[3][3] == "x" && board[4][4] == "x" {
+    if board[0][0] == -1 && board[1][1] == -1 && board[2][2] == -1 && board[3][3] == -1 && board[4][4] == -1 {
         return true
     }
     // bottom left to top right
-    if board[4][0] == "x" && board[3][1] == "x" && board[2][2] == "x" && board[1][3] == "x" && board[0][4] == "x" {
+    if board[4][0] == -1 && board[3][1] == -1 && board[2][2] == -1 && board[1][3] == -1 && board[0][4] == -1 {
         return true
     }
     return false
@@ -129,32 +145,29 @@ func checkDiagonal(board [][]string) bool {
 
 // checkWinner check if a bingo board has won
 // and if so calculate the winning score and return it
-func checkWinner(board [][]string, lastNumber string) int {
-    score := -1
-
+func checkWinner(board [][]int, lastNumber int) int {
     for x := 0; x < 5; x++ {
         for y := 0; y < 5; y++ {
             // we have the number drawn
             if board[x][y] == lastNumber {
-                board[x][y] = "x"
-                // check for horizontal bingo
-                if checkHorizontal(board) {
-                    return getScore(board, lastNumber)
-                }
+                board[x][y] = -1
                 // check for vertical bingo
                 if checkVertical(board) {
+                    return getScore(board, lastNumber)
+                }
+                // check for horizontal bingo
+                if checkHorizontal(board) {
                     return getScore(board, lastNumber)
                 }
                 // check for diagonal bingo
                 // if checkDiagonal(board) {
                 //     return getScore(board, lastNumber)
                 // }
-                return score
             }
         }
     }
 
-    return score
+    return -1
 }
 
 func main() {
@@ -162,29 +175,21 @@ func main() {
 
     drawn, boards := readInput()
 
-    // clean up white spaces in boards + drawn list
-    for _, board := range(boards) {
-        for x := 0; x < 5; x++ {
-            for y := 0; y < 5; y++ {
-                board[x][y] = strings.ReplaceAll(board[x][y], " ", "")
-            }
-        }
-
-    }
-    for i, num := range(drawn) {
-        drawn[i] = strings.ReplaceAll(num, " ", "")
-    }
-
     // parse through drawn numbers and
     // find the bingo board that wins it all
     for _, num := range(drawn) {
-        fmt.Printf("Number drawn: %s\n", num)
+        fmt.Printf("Number drawn: %d\n", num)
+        score := -1
         for i, board := range(boards) {
-            score := checkWinner(board, num)
+            score = checkWinner(board, num)
             if score != -1 {
+                for x, b := range(boards) {
+                    fmt.Printf("Board %d\n", x + 1)
+                    viewBoard(b)
+                    fmt.Println("----------")
+                }
                 fmt.Printf("Winner: Board %d\n", i + 1)
                 fmt.Printf("Score: %d\n", score)
-                viewBoard(board)
                 os.Exit(0)
             }
         }
