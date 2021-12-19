da7b7aab42830bc8142a2d4797c8aa027ef4722f
Christopher Raley <cr2fv@virginia.edu>
2021-12-18 22:15:55 -0500

    add day 14 part 2 solution

--- day14/part2/main.go
+++ day14/part2/main.go
@@ -38,40 +38,9 @@ func main() {
         log.Fatal(err)
     }
 
-    polymer := []rune(input)
-
-    steps := 10
-    for i := 0; i < steps; i++ {
-        pairs := make(map[string][]int) // hash mapping pairs to a list of their indexes (location)
-        for x := 0; x < len(polymer) - 1; x++ {
-            pair := string(polymer[x]) + string(polymer[x + 1])
-            pairs[pair] = append(pairs[pair], x + 1)
-        }
-        for key, indexes := range(pairs) {
-            if hash[key] != "" {
-                // for each instance of the pair
-                for _, index := range(indexes) {
-                    val := []rune(hash[key]) // split hash entry into 2 characters
-                    polymer = append(polymer[:index + 1], polymer[index:]...) // add extra space for new character
-                    polymer[index] = val[0] // replace new spot with entry in hash
-
-                    for k, in := range(pairs) {
-                        for slot, v := range(in){
-                            if v > index {
-                                pairs[k][slot] += 1
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     count := make(map[string]int)
 
-    for _, s := range(polymer) {
-        count[string(s)] += 1
-    }
+    count = polymerize(40, input, hash)
 
     ordered := []int{}
     for _, val := range(count) {
@@ -80,5 +49,48 @@ func main() {
 
     sort.Ints(ordered)
 
-    fmt.Printf("%d\n", ordered[len(ordered) - 1] - ordered[0])
+    fmt.Printf("Solution: %d\n", ordered[len(ordered) - 1] - ordered[0])
+}
+
+func polymerize(steps int, polymer string, hash map[string]string) map[string]int {
+    totalPairs := make(map[string]int)
+
+    // get all pairs
+    for x := 0; x < len(polymer) - 1; x++ {
+        pair := string(polymer[x]) + string(polymer[x + 1])
+        totalPairs[pair] += 1
+    }
+
+    for i := 0; i < steps; i++ {
+        perIteration := make(map[string]int)
+        for pair, pairTotal := range(totalPairs) {
+            if pairTotal != 0 {
+                totalPairs[pair] -= pairTotal
+                insertionElement := hash[pair]
+
+                leftPair := string(pair[0]) + insertionElement
+                rightPair := insertionElement + string(pair[1])
+
+                perIteration[leftPair] += pairTotal
+                perIteration[rightPair] += pairTotal
+            }
+        }
+
+        // update
+        for pair, pairTotal := range(perIteration) {
+            totalPairs[pair] = pairTotal
+        }
+    }
+
+    count := make(map[string]int) // holds mapping for letter to total occurrences
+
+    for pair, pairTotal := range(totalPairs) {
+        if pairTotal != 0 {
+            count[string(pair[0])] += pairTotal
+        }
+    }
+    
+    count[string(polymer[len(polymer) - 1])] += 1
+
+    return count
 }
