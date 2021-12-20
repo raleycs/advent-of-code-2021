be2d800b1af7c84a29a6b8bd055915a36853ff92
Christopher Raley <cr2fv@virginia.edu>
2021-12-20 03:55:48 -0500

    add day 16 part 2 solution

--- day16/part2/main.go
+++ day16/part2/main.go
@@ -6,152 +6,137 @@ import(
     "log"
     "os"
     "strconv"
-    "strings"
 )
 
-func parseBinary(binary string) {
-    var sum int64
-    sum = 0
+func doOp(id int64, values []int64) int64 {
+    // sum
+    if id == 0 {
+        var sum int64
+        for _, v := range(values) {
+            sum += v
+        }
+        return sum
+    }
+    // product
+    if id == 1 {
+        var total = values[0]
+        for i := 1; i < len(values); i++ {
+            total *= values[i]
+        }
+        return total
+    }
+    // min
+    if id == 2 {
+        var min = values[0]
+        for i := 1; i < len(values); i++ {
+            if min > values[i] {
+                min = values[i]
+            }
+        }
+        return min
+    }
+    // max
+    if id == 3 {
+        var max = values[0]
+        for i := 1; i < len(values); i++ {
+            if max < values[i] {
+                max = values[i]
+            }
+        }
+        return max
+    }
+    // literal
+    if id == 4 {
+        return values[0]
+    }
+    // greater than
+    if id == 5 {
+        if values[0] > values[1] {
+            return 1
+        } else {
+            return 0
+        }
+    }
+    // less than
+    if id == 6 {
+        if values[0] < values[1] {
+            return 1
+        } else {
+            return 0
+        }
+    }
+    // equal to
+    if id == 7 {
+        if values[0] == values[1] {
+            return 1
+        } else {
+            return 0
+        }
+    }
+    return 99999
+}
 
-    // parse until we reach the end
-    // for len(binary) >= 1 {
-
-    version, id := getHeader(binary)
-    sum += version
+func parseBinary(binary string, index int64) (int64, int64) {
+    var results []int64
+    id, err := strconv.ParseInt(binary[index + 3:index + 6], 2, 64)
+    if err != nil {
+        log.Fatal(err)
+    }
+    index += 6
 
     // literal value
     if id == 4 {
-        var literal int64
-        var size int64
-        literal, size = parseLiteral(binary)
-        fmt.Printf("Literal extracted: %d\n", literal)
-        for size % 8 != 0 {
-            size += 1
-        }
-    } else {
-        // operator
-        versionTotal, _ := parseOp(binary)
-        sum += versionTotal
-    }
-    fmt.Printf("Sum of versions: %d\n", sum)
-    fmt.Println("--------------------")
-}
-
-func getHeader(binary string) (int64, int64) {
-    // extract header
-    version := binary[0:3]
-    versionInt, err := strconv.ParseInt(version, 2, 64)
-    if err != nil {
-        log.Fatal(err)
-    }
-    id := binary[3:6]
-    idInt, err := strconv.ParseInt(id, 2, 64)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    return versionInt, idInt
-}
-
-func parseOp(binary string) (int64, int64) {
-    var sum int64
-    var packetSize int64
-    lengthType := binary[6]
-
-    // next 15 bits are a number that represents the total length in bits of the sub-packets
-    if string(lengthType) == "0" {
-        length, err := strconv.ParseInt(binary[7:22], 2, 64)
-        if err != nil {
-            log.Fatal(err)
-        }
-        var total int64
-        binary = binary[22:] // truncate header + length bits
-        packetSize += 22
+        results = []int64{0}
 
         for {
-            version, id := getHeader(binary)
-            sum += version
-
-            if id == 4 {
-                literal, size := parseLiteral(binary)
-                fmt.Printf("Literal extracted: %d\n", literal)
-                binary = binary[size:]
-                total += size
-                packetSize += size
-            } else {
-                versionTotal, size := parseOp(binary)
-                binary = binary[size:]
-                sum += versionTotal
-                total += size
-                packetSize += size
+            results[0] *= 16
+            val, err := strconv.ParseInt(binary[index + 1:index + 5], 2, 64)
+            if err != nil {
+                log.Fatal(err)
             }
-            if total == length {
-                break
+            results[0] += val
+            index += 5
+
+            if binary[index - 5] == '0' {
+                return index, doOp(id, results)
             }
         }
-    } else if string(lengthType) == "1" {
-        // next 11 bits are a number that represents the number of sub-packets immediately contained by this packet
-        length, err := strconv.ParseInt(binary[7:18], 2, 64)
-        if err != nil {
-            log.Fatal(err)
-        }
+    } else {
+        results = []int64{}
+        if binary[index] == '0' {
+            endIndex, err := strconv.ParseInt(binary[index + 1:index + 16], 2, 64)
+            if err != nil {
+                log.Fatal(err)
+            }
+            endIndex += index + 16
+            index += 16
 
-        binary = binary[18:] // truncate header + length bits
-        var parsed int64
+            var res int64
+            for index < endIndex {
+                index, res = parseBinary(binary, index)
+                results = append(results, res)
+            }
+            return index, doOp(id, results)
+        } else {
+            numPackets, err := strconv.ParseInt(binary[index + 1:index + 12], 2, 64)
+            if err != nil {
+                log.Fatal(err)
+            }
+            index += 12
 
-        packetSize += 18
-
-        for i := int64(0); i < length; i++ {
-            version, id := getHeader(binary)
-            sum += version
-
-            if id == 4 {
-                literal, size := parseLiteral(binary)
-                fmt.Printf("Literal extracted: %d\n", literal)
-                binary = binary[size:]
-                parsed += size
-                packetSize += size
-            } else {
-                versionTotal, size := parseOp(binary)
-                sum += versionTotal
-                binary = binary[size:]
-                parsed += size
-                packetSize += size
+            var res int64
+            for i := int64(0); i < numPackets; i++ {
+                index, res = parseBinary(binary, index)
+                results = append(results, res)
             }
         }
+        return index, doOp(id, results)
     }
-    return sum, packetSize
-}
-
-func parseLiteral(binary string) (int64, int64) {
-    groups := []string{} // group of binary to put together literal
-    packet := binary[6:] // skip header
-    i := 1
-    var size int64
-    size = 6
-    // parse 3 groups of bits
-    for i != 0 {
-        groups = append(groups, packet[(i - 1) * 5:i * 5][1:])
-        size += 5
-
-        // last group
-        if string(packet[(i - 1) * 5:i * 5][0]) == "0" {
-            break
-        }
-        i += 1
-    }
-
-    literal, err := strconv.ParseInt(strings.Join(groups, ""), 2, 64)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return literal, size
 }
 
 func main() {
     // Open "input.txt"
     buffer, err := os.Open("input.txt")
-    // buffer, err := os.Open("test.txt")
     if err != nil {
         log.Fatal(err)
     }
@@ -183,7 +168,7 @@ func main() {
             }
             binary += bin
         }
-
-        parseBinary(binary)
+        _, result := parseBinary(binary, int64(0))
+        fmt.Printf("Solution: %d\n", result)
     }
 }
